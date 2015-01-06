4b90d9b044184979096dc11dfc63f149201a8d4e
Jason Trader <jTrader17@my.whitworth.edu>
2015-01-06 14:59:01 -0800

    Second part done

---
+++ HW_1/PR1_2/src/pr1_2/PR1_2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pr1_2;
+
+/**
+ *
+ * @author jTrader17
+ */
+public class PR1_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.printf("First string: %s\n", args[0]);
+        System.out.printf("Second string: %s\n", args[1]);
+        System.out.printf("Third string: %s\n", args[2]);
+    }
+    
+}
