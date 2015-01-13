1f4b5a0c2f28e60ba536a7f4abedade01fe0a81c
Jason Trader <jTrader17@my.whitworth.edu>
2015-01-12 16:00:04 -0800

    Started part 2

---
+++ HW_2/PR3_2/src/pr3_2/Shape.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pr3_2;
+
+/**
+ *
+ * @author jtrader17
+ */
+public class Shape {
+    
+}
